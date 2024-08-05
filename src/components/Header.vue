ac1459314a47cf889dbe7568db2789e8737feb1a
pn6777 <pn6777@gmail.com>
2024-08-05 11:18:06 +0800

    frist Revise

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+
+    <div class="header">
+        <a href=""><img src="../../font.png" alt=""></a>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </div>
+
+</template>
+
+<style scpoed lang="scss">
+body{
+    background-color: #FFFBE2;
+}
+
+.header{
+    height: 10vh;
+    padding: 0 13vw;
+    position: fixed;
+    top: 0%;
+    background-color: pink;
+    z-index: 1;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="topPage">
+        <div class="imgDiv">
+          <div class="maskDiv"></div>
+        </div>
+    </div>
+    <!-- <div class="bottomDiv"></div> -->
+  </main>
+</template>
+
+<style scoped lang="scss">
+.topPage {
+  width: 100vw;
+  height: 70vh;
+  margin-top: 20vh;
+
+  .maskDiv{
+    width: 100vw;
+    height: 70vh;
+    backdrop-filter: blur(5px);
+  }
+
+  .imgDiv {
+    width: 100vw;
+    height: 70vh;
+    margin-top: 20vh;
+    background-image: url("https://www.ikea.com.tw/dairyfarm/tw/pageImages/page_prev_1868_zh_tw_16866744371681051022.jpeg");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+  }
+
+}
+.bottomDiv{
+  height: 100vh;
+}
+</style>
